8f19bc55fa40c79c39efc9bdff17f4650cf10f9a
Daniel Silveira <murtdaniel@gmail.com>
2022-11-18 17:34:41 -0300

    Cadastro de Clientes [Início]

--- Defs.py
+++ Defs.py
@@ -6,6 +6,7 @@ def cabeçalho():
     print('[4] Sair')
     print('-'*34)
 
+
 def CadastrarCliente():
     print('[Forneça as informações do Cliente]')
     nome = str(input('Nome: '))
@@ -15,3 +16,24 @@ def CadastrarCliente():
     matricula = int(input('Matrícula: '))
     print('[Cadastro Concluído]')
 
+
+def CadastrarEndereçoCliente():
+    usuario = str(input('Deseja cadastrar o endereço de qual usuário?: '))
+    print('[Forneça o Endereço do usuário]')
+    rua = str(input('Rua: '))
+    numero_casa_ap = str(input('Nº Casa ou Apartamento: '))
+    complemento = str(input('Complemento [Opcional]: '))
+    bairro = str(input('Bairro: '))
+    cidade = str(input('Cidade: '))
+    estado = str(input('Estado: '))
+
+
+def ConsultarCliente():
+    nome_cliente = str(input('Nome do ciente: '))
+
+
+def SairCadastro():
+    from time import sleep
+    print('Saindo do sistema, aguarde...')
+    sleep(3)
+    print('Sistema finalizado com sucesso!S')
--- Main.py
+++ Main.py
@@ -10,6 +10,26 @@ while True:
             if r == 'N':
                 break
 
+    if opcao == 2:
+        while True:
+            Defs.CadastrarEndereçoCliente()
+            r = str(input('Deseja cadastrar outro endereço?[S/N]: ')).strip().upper()
+            if r == 'N':
+                break
+
+    if opcao == 3:
+        while True:
+            Defs.ConsultarCliente()
+            r = str(input('Deseja consultar outro cliente?[S/N]: ')).strip().upper()
+            if r == 'N':
+                break
+
+
+    if opcao == 4:
+        Defs.SairCadastro()
+        break
+
+
 
 
 
